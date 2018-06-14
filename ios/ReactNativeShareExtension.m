7d9f7f9b0785fa4f45cf7b472fd99086266e2a4b
Marcin Olek <marcin@spotio.com>
2018-06-14 15:43:05 +0200

    handle multiple provider + parse Apple Maps and Google Maps urls + send address to React

--- ios/ReactNativeShareExtension.m
+++ ios/ReactNativeShareExtension.m
@@ -5,6 +5,8 @@
 
 #define URL_IDENTIFIER @"public.url"
 #define IMAGE_IDENTIFIER @"public.image"
+#define VCARD_IDENTIFIER @"public.vcard"
+#define MAP_IDENTIFIER @"com.apple.mapkit.map-item"
 #define TEXT_IDENTIFIER (NSString *)kUTTypePlainText
 
 NSExtensionContext* extensionContext;
@@ -65,110 +67,165 @@ RCT_REMAP_METHOD(data,
     }];
 }
 
+- (NSString *)findGoogleAddressIn:(NSString *)url {
+    NSRange searchedRange = NSMakeRange(0, [url length]);
+    NSString *pattern = @"/?q=(([^&]+))&";
+    NSError  *error = nil;
+    NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern: pattern options:0 error:&error];
+    NSArray* matches = [regex matchesInString:url options:0 range: searchedRange];
+    for (NSTextCheckingResult* match in matches) {
+        NSString* matchText = [url substringWithRange:[match range]];
+        NSString *address = [matchText substringWithRange:NSMakeRange(2, matchText.length - 3)]; // ommit q= and final &
+        return address;
+    }
+    return nil;
+}
+
+- (NSString *)findAppleAddressIn:(NSString *)url {
+    NSRange searchedRange = NSMakeRange(0, [url length]);
+    NSString *pattern = @"/?address=(([^&]+))&";
+    NSError  *error = nil;
+    NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern: pattern options:0 error:&error];
+    NSArray* matches = [regex matchesInString:url options:0 range: searchedRange];
+    for (NSTextCheckingResult* match in matches) {
+        NSString* matchText = [url substringWithRange:[match range]];
+        NSString *address = [matchText substringWithRange:NSMakeRange(8, matchText.length - 9)]; // ommit q= and final &
+        return address;
+    }
+    return nil;
+}
+
+- (NSString *)findAppleCoordinates:(NSString *)url {
+    NSRange searchedRange = NSMakeRange(0, [url length]);
+    NSString *pattern = @"/?ll=(([^&]+))&";
+    NSError  *error = nil;
+    NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern: pattern options:0 error:&error];
+    NSArray* matches = [regex matchesInString:url options:0 range: searchedRange];
+    for (NSTextCheckingResult* match in matches) {
+        NSString* matchText = [url substringWithRange:[match range]];
+        NSString *address = [matchText substringWithRange:NSMakeRange(3, matchText.length - 4)]; // ommit q= and final &
+        return address;
+    }
+    return nil;
+}
+    
 - (void)extractDataFromContext:(NSExtensionContext *)context withCallback:(void(^)(NSString *value, NSString* contentType, NSException *exception))callback {
     
     
     void (^processResults)(NSArray *) = ^void(NSArray *results) {
         for (DataItem *result in results) {
-            NSLog(@"Provider Result %@ Value: %@", result.contentType, result.value);
+            NSLog(@"Share Extension Provider Result %@ Value: %@", result.contentType, result.value);
+            if(!result.exception) {
+                // Google
+                if([result.value containsString:@"goo.gl"]) {
+                    NSURL *redirectURL = [NSURL URLWithString:result.value];
+                    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:redirectURL cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:15.0];
+                    [request setHTTPMethod:@"HEAD"];
+                    NSURLSession *session = [NSURLSession sharedSession];
+                    [[session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+                        if(!error) {
+                            NSString *address = [self findGoogleAddressIn:response.URL.absoluteString];
+                            NSString *response = [NSString stringWithFormat:@"address=%@", address];
+                            callback(response, @"text/plain", nil);
+                        }
+                    }] resume];
+                // Apple
+                } else if([result.value containsString:@"maps.apple.com"]) {
+                    NSString *url = result.value;
+                    NSString *address = [self findAppleAddressIn:url];
+                    NSString *coordinates = [self findAppleCoordinates:url];
+                    NSString *response = [NSString stringWithFormat:@"address=%@&coords=%@", address, coordinates];
+                    callback(response, @"text/plain", nil);
+                }
+            }
         }
-        /* if(callback) {
-         callback([url absoluteString], @"text/plain", nil);
-         } */
-        
-        /* if(callback) {
-         callback(fullPath, [fullPath pathExtension], nil);
-         } */
+
     };
     
     @try {
         NSExtensionItem *item = [context.inputItems firstObject];
         NSArray *attachments = item.attachments;
         
-        __block NSItemProvider *urlProvider = nil;
-        __block NSItemProvider *imageProvider = nil;
-        __block NSItemProvider *textProvider = nil;
-        
-        NSMutableArray *providers = [NSMutableArray array];
+        NSInteger itemsCount = [attachments count];
+        __block NSMutableArray *results = [NSMutableArray array];
         
         [attachments enumerateObjectsUsingBlock:^(NSItemProvider *provider, NSUInteger idx, BOOL *stop) {
-            if([provider hasItemConformingToTypeIdentifier:URL_IDENTIFIER]) {
-                urlProvider = provider;
-                [providers addObject:urlProvider];
+            
+            if ([provider hasItemConformingToTypeIdentifier:URL_IDENTIFIER]){
+                [provider loadItemForTypeIdentifier:URL_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                    NSURL *url = (NSURL *)item;
+                    DataItem *dataItem = [DataItem itemWithContentType:@"text/plain" value:[url absoluteString] exception:nil];
+                    [results addObject:dataItem];
+                    if(results.count == itemsCount) {
+                        processResults(results);
+                    }
+                }];
+                
             } else if ([provider hasItemConformingToTypeIdentifier:TEXT_IDENTIFIER]){
-                textProvider = provider;
-                [providers addObject:urlProvider];
-            } else if ([provider hasItemConformingToTypeIdentifier:IMAGE_IDENTIFIER]){
-                imageProvider = provider;
-                [providers addObject:urlProvider];
+                
+                [provider loadItemForTypeIdentifier:TEXT_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                    NSString *text = (NSString *)item;
+                    DataItem *dataItem = [DataItem itemWithContentType:@"text/plain" value:text exception:nil];
+                    [results addObject:dataItem];
+                    if(results.count == itemsCount) {
+                        processResults(results);
+                    }
+                }];
+                
+            } else  /* if ([provider hasItemConformingToTypeIdentifier:MAP_IDENTIFIER]){
+                
+                [provider loadItemForTypeIdentifier:MAP_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                    NSLog(@"GOT");
+                }];
+                
+            } else if ([provider hasItemConformingToTypeIdentifier:VCARD_IDENTIFIER]){
+                
+                [provider loadItemForTypeIdentifier:VCARD_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                    NSLog(@"GOT");
+                }];
+                
+            }
+            
+            else  if ([provider hasItemConformingToTypeIdentifier:IMAGE_IDENTIFIER]){
+                
+                [provider loadItemForTypeIdentifier:IMAGE_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                    
+                    UIImage *sharedImage;
+                    NSString *filePath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"RNSE_TEMP_IMG"];
+                    NSString *fullPath = [filePath stringByAppendingPathExtension:@"png"];
+                    
+                    if ([(NSObject *)item isKindOfClass:[UIImage class]]){
+                        sharedImage = (UIImage *)item;
+                    }else if ([(NSObject *)item isKindOfClass:[NSURL class]]){
+                        NSURL* url = (NSURL *)item;
+                        NSData *data = [NSData dataWithContentsOfURL:url];
+                        sharedImage = [UIImage imageWithData:data];
+                    }
+                    
+                    [UIImagePNGRepresentation(sharedImage) writeToFile:fullPath atomically:YES];
+                    
+                    DataItem *dataItem = [DataItem itemWithContentType:[fullPath pathExtension] value:fullPath exception:nil];
+                    [results addObject:dataItem];
+                    if(results.count == itemsCount) {
+                        processResults(results);
+                    }
+                    
+                }];
+            } else */ {
+                DataItem *dataItem = [DataItem itemWithContentType:nil value:nil exception: [NSException exceptionWithName:@"Error" reason:@"couldn't find provider" userInfo:nil]];
+                [results addObject:dataItem];
+                if(results.count == itemsCount) {
+                    processResults(results);
+                }
             }
         }];
         
         
-        NSInteger itemsCount = [providers count];
-        __block NSMutableArray *results = [NSMutableArray array];
-        
-        
-        if(urlProvider) {
-            [urlProvider loadItemForTypeIdentifier:URL_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
-                NSURL *url = (NSURL *)item;
-                DataItem *dataItem = [DataItem itemWithContentType:@"text/plain" value:[url absoluteString] exception:nil];
-                [results addObject:dataItem];
-                if(results.count == itemsCount) {
-                    processResults(results);
-                }
-            }];
-        } else if (imageProvider) {
-            [imageProvider loadItemForTypeIdentifier:IMAGE_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
-                
-                /**
-                 * Save the image to NSTemporaryDirectory(), which cleans itself tri-daily.
-                 * This is necessary as the iOS 11 screenshot editor gives us a UIImage, while
-                 * sharing from Photos and similar apps gives us a URL
-                 * Therefore the solution is to save a UIImage, either way, and return the local path to that temp UIImage
-                 * This path will be sent to React Native and can be processed and accessed RN side.
-                 **/
-                
-                UIImage *sharedImage;
-                NSString *filePath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"RNSE_TEMP_IMG"];
-                NSString *fullPath = [filePath stringByAppendingPathExtension:@"png"];
-                
-                if ([(NSObject *)item isKindOfClass:[UIImage class]]){
-                    sharedImage = (UIImage *)item;
-                }else if ([(NSObject *)item isKindOfClass:[NSURL class]]){
-                    NSURL* url = (NSURL *)item;
-                    NSData *data = [NSData dataWithContentsOfURL:url];
-                    sharedImage = [UIImage imageWithData:data];
-                }
-                
-                [UIImagePNGRepresentation(sharedImage) writeToFile:fullPath atomically:YES];
-                
-                DataItem *dataItem = [DataItem itemWithContentType:[fullPath pathExtension] value:fullPath exception:nil];
-                [results addObject:dataItem];
-                if(results.count == itemsCount) {
-                    processResults(results);
-                }
-                
-            }];
-        } else if (textProvider) {
-            [textProvider loadItemForTypeIdentifier:TEXT_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
-                NSString *text = (NSString *)item;
-                DataItem *dataItem = [DataItem itemWithContentType:@"text/plain" value:text exception:nil];
-                [results addObject:dataItem];
-                if(results.count == itemsCount) {
-                    processResults(results);
-                }
-            }];
-        } else {
-            if(callback) {
-                callback(nil, nil, [NSException exceptionWithName:@"Error" reason:@"couldn't find provider" userInfo:nil]);
-            }
-        }
     }
     @catch (NSException *exception) {
-        if(callback) {
-            callback(nil, nil, exception);
-        }
+        DataItem *dataItem = [DataItem itemWithContentType:nil value:nil exception: exception];
+        NSArray *results = @[dataItem];
+        processResults(results);
     }
 }
 
